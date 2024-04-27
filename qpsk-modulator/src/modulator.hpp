835f22e0033f2bf1a4d5ef9f3fed2143c8f6fee3
Eduard Yablonskyy <eduard.yablonskyy@viapath.com>
2024-04-28 01:09:44 +0300

    modulator produces empty output when input is empty

---
+++ qpsk-modulator/src/modulator.hpp
@@ -0,0 +1,8 @@
+namespace qpsk {
+  struct symbol_t {
+  };
+ 
+  template <typename I, typename O>
+  void modulate(I first, I last, O destination) {
+  }
+}
--- qpsk-modulator/test/modulator_test.cpp
+++ qpsk-modulator/test/modulator_test.cpp
@@ -1,7 +1,20 @@
 #include <gmock/gmock.h>
+#include <iterator>
+#include <vector>
 
 #include "modulator.hpp"
 
-TEST(modulator, dummy) {
-  EXPECT_EQ(1, 1);
+namespace {
+  using std::back_inserter;
+  using std::begin;
+  using std::end;
+  using std::vector;
+  using testing::IsEmpty;
+
+  TEST(modulator, produces_empty_output_when_input_is_empty) {
+    vector<char> input{};
+    vector<qpsk::symbol_t> symbols{};
+    qpsk::modulate(begin(input), end(input), back_inserter(symbols));
+    EXPECT_THAT(symbols, IsEmpty());
+  }
 }
