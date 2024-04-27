36de2d12440803de5acdbf83ea0aa6de19992214
Eduard Yablonskyy <eduard.yablonskyy@viapath.com>
2024-04-28 02:02:39 +0300

    modulate produces one correct symbol for single pair 00

--- qpsk-modulator/src/modulator.hpp
+++ qpsk-modulator/src/modulator.hpp
@@ -1,8 +1,25 @@
+#include <ostream>
+#include <numbers>
+
 namespace qpsk {
   struct symbol_t {
+    float in_phase;
+    float quadrature;
+
+    bool operator==(symbol_t const& rhs) const {
+      return in_phase == rhs.in_phase and quadrature == rhs.quadrature;
+    }
+
+    friend std::ostream& operator<<(std::ostream& output, symbol_t const& symbol) {
+      return output << "(I" << symbol.in_phase << ", Q" << symbol.quadrature << ")";
+    }
   };
  
   template <typename I, typename O>
   void modulate(I first, I last, O destination) {
+    using std::numbers::sqrt2;
+    if (first != last) {
+      *destination = symbol_t{in_phase: 1/sqrt2, quadrature: 1/sqrt2};
+    }
   }
 }
--- qpsk-modulator/test/modulator_test.cpp
+++ qpsk-modulator/test/modulator_test.cpp
@@ -1,6 +1,7 @@
 #include <gmock/gmock.h>
 #include <iterator>
 #include <vector>
+#include <numbers>
 
 #include "modulator.hpp"
 
@@ -10,11 +11,20 @@ namespace {
   using std::end;
   using std::vector;
   using testing::IsEmpty;
+  using testing::ElementsAre;
+  using std::numbers::sqrt2;
 
   TEST(modulate, produces_empty_output_when_input_is_empty) {
     vector<char> input{};
     vector<qpsk::symbol_t> symbols{};
     qpsk::modulate(begin(input), end(input), back_inserter(symbols));
     EXPECT_THAT(symbols, IsEmpty());
+  }
+
+  TEST(modulate, produces_one_correct_symbol_for_single_pair_00) {
+    auto input = {"00"};
+    vector<qpsk::symbol_t> symbols{};
+    qpsk::modulate(begin(input), end(input), back_inserter(symbols));
+    EXPECT_THAT(symbols, ElementsAre(qpsk::symbol_t{in_phase: 1/sqrt2, quadrature: 1/sqrt2}));
   }
 }
